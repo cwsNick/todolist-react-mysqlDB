b91b9cb9355fc85e4acb839efc2886e39359e035
Nick Chong <cwsnick@outlook.com>
2022-06-05 22:09:34 +0800

    using dotenv

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
-
+const dotenv = require("dotenv");
+dotenv.config()
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.use(cors());
